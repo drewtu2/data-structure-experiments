995ca5405ab2457e411e6ec8db4699550bdf3396
Andrew Tu <drewtu2@yahoo.com>
2020-06-24 02:15:03 -0400

    updates to testing scripts

--- shared_ptr_test/main.cpp
+++ shared_ptr_test/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <string.h>
 
 void DoSomethingPtr(const std::shared_ptr<const std::vector<int>> vec) {
+    std::vector<int> test{vec->begin(), vec->end()};
     return;
 }
 
 void DoSomethingReference(const std::shared_ptr<const std::vector<int>>& vec) {
+    std::vector<int> test{vec->begin(), vec->end()};
     return;
 }
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if(argc != 2) {
+        std::cout << "usage: main <ptr/ref>" << std::endl;
+    } 
+
     srand((unsigned)time(NULL));
     const int vec_size = 10;
+    const int num_runs = 10 * 1000 * 1000;
 
+    // Create a random vector. 
     std::vector<int> my_vec;
     my_vec.reserve(vec_size);
     for (int ii = 0; ii < vec_size; ++ii) {
@@ -23,11 +33,20 @@ int main() {
     }
     auto ptr = std::make_shared<const std::vector<int>>(my_vec);
 
-    const int num_runs = 10 * 1000 * 1000;
-    for(int ii = 0; ii < num_runs; ++ii) {
-        //DoSomethingPtr(ptr);
-        DoSomethingReference(ptr);
+
+    // Decide which version to run.
+    if (strcmp(argv[1], "ptr") == 0) {
+        std::cout << "running " << argv[1] << std::endl;
+        for(int ii = 0; ii < num_runs; ++ii) {
+            DoSomethingPtr(ptr);
+        }
+    } else if(strcmp(argv[1], "ref") == 0) {
+        std::cout << "running " << argv[1] << std::endl;
+        for(int ii = 0; ii < num_runs; ++ii) {
+            DoSomethingReference(ptr);
+        }
     }
+
 
     return 0;
 }
